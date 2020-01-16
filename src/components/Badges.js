b42c2b4b3b2c1304ba309fcb4c602be6e8ec3183
Sebastian Coscia <sebascoscia@gmail.com>
2020-01-16 00:04:15 -0300

    agregado botones home

--- src/components/Badges.js
+++ src/components/Badges.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import newTicket from '../components/img/add_document.png'
 import './styles/Badges.css'
 
 export default class Badges extends Component {
@@ -13,7 +12,7 @@ export default class Badges extends Component {
 														<div class="card-header h6 bg-primary text-white">{this.props.title}</div>
 															<a class="card-body link-container"  href="new">
 																<div class="row align-items-center">
-																		<img class='imgIndex' src={newTicket} alt={this.props.alt} />
+																		<img class='imgIndex' src={this.props.TicketImg} alt={this.props.alt} />
 																</div>
 															</a>
 													</div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,27 @@
 import React from 'react'
+import newTicketImg from '../components/img/add_document.png'
+import sendTicketImg from '../components/img/send.png'
+import getTicketImg from '../components/img/recibido.png'
 import Badges from '../components/Badges'
 import Navbar from '../components/Navbar'
 
 class Home extends React.Component {
+    
     render() {
+      
         return (
             <React.Fragment>
-                
+              
                <Navbar/>
-               <div className="container px-5 mt-5">
-                <Badges title="Crear Nuevo ticket" alt="Nuevo ticket"/>
+               <div className="">
+                <div className="container px-5 mt-4 d-flex justify-content-between">
+
+                <Badges TicketImg={newTicketImg} title="Crear Nuevo ticket" alt="Nuevo ticket"/>
+                <Badges TicketImg={sendTicketImg} title="Tickets Enviados" alt="Tickets Enviados"/>
+                <Badges TicketImg={getTicketImg} title="TIckets Recibidos" alt="Tickets Recibidos"/>
+
+                </div>
+                
                </div>
                
              
